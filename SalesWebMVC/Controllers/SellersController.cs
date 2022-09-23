567d858ee25613b80f0b9dfcde46b882656bdd99
Michel Versiani <michel.pereira@audaztec.com.br>
2022-09-23 18:38:19 -0300

    New - Adding Sellers controller and view

---
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+using SalesWebMVC.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMVC.Controllers
+{
+    public class SellersController : Controller
+    {
+        private readonly SellerService _service;
+
+        public SellersController(SellerService service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Index()
+        {
+            List<Seller> sellers = _service.FindAll();
+            return View(sellers);
+        }
+    }
+}
