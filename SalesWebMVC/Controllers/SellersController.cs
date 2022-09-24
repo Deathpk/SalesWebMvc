6cd328e3dd90bdde0e1b8e2e970cc51cf694a5be
versianidev <bettercallmiguel@gmail.com>
2022-09-24 19:31:47 -0300

    New - Adding the Create endpoint for Seller's Entity.

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -22,5 +22,18 @@ namespace SalesWebMVC.Controllers
             List<Seller> sellers = _service.FindAll();
             return View(sellers);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _service.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
