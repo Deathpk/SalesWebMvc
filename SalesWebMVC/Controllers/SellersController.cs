9e00e4985bbea414383fcff609ca5198f6685cce
Michel Versiani <michel.pereira@audaztec.com.br>
2022-09-26 15:20:33 -0300

    New - Adding the edit method for Seller Entity

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -2,10 +2,8 @@
 using SalesWebMVC.Models;
 using SalesWebMVC.Models.ViewModels;
 using SalesWebMVC.Services;
-using System;
+using SalesWebMVC.Services.Exceptions;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SalesWebMVC.Controllers
 {
@@ -22,7 +20,7 @@ namespace SalesWebMVC.Controllers
 
         public IActionResult Index()
         {
-            List<Seller> sellers = _service.FindAll();
+            List<Seller> sellers = _service.FindAllWithRelations();
             return View(sellers);
         }
 
@@ -38,6 +36,94 @@ namespace SalesWebMVC.Controllers
         public IActionResult Create(Seller seller)
         {
             _service.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Seller seller = _service.FindById(id.Value);
+
+            if (seller == null)
+            {
+                return NotFound();
+            }
+
+            return View(seller);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _service.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Seller seller = _service.FindById(id.Value);
+
+            if (seller == null)
+            {
+                return NotFound();
+            }
+
+            return View(seller);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Seller seller = _service.FindById(id.Value);
+
+            if (seller == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = _departmentService.FindAll();
+            SellerFormViewModel viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(int? id , Seller seller)
+        {
+            if (id != seller.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                _service.Update(seller);
+            } 
+            catch(NotFoundException)
+            {
+                return NotFound();
+            }
+            catch(DbConcurrencyException)
+            {
+                return BadRequest();
+            }
+
             return RedirectToAction(nameof(Index));
         }
     }
--- SalesWebMVC/Services/SellerService.cs
+++ SalesWebMVC/Services/SellerService.cs
@@ -1,5 +1,7 @@
-﻿using SalesWebMVC.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Data;
 using SalesWebMVC.Models;
+using SalesWebMVC.Services.Exceptions;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -19,10 +21,53 @@ namespace SalesWebMVC.Services
             return _context.Seller.ToList();
         }
 
+        public List<Seller> FindAllWithRelations()
+        {
+            return _context.Seller.Include("Department").ToList();
+        }
+
         public void Insert(Seller seller)
         {
             _context.Add(seller);
             _context.SaveChanges();
+        }
+
+        public Seller FindById(int sellerId)
+        {
+            //TODO LANÇAR EXCEÇÃO DEPOIS...
+            return _context.Seller.Include("Department")
+                .FirstOrDefault(seller => seller.Id == sellerId);
+        }
+
+        public void Remove(int sellerId)
+        {
+            Seller seller = FindById(sellerId);
+
+            List<SalesRecord> sales = _context.SalesRecord.ToList();
+            _context.SalesRecord.RemoveRange(sales);
+
+            _context.Seller.Remove(seller);
+            _context.SaveChanges();
+        }
+
+        public void Update(Seller seller)
+        {
+            bool sellerExists = _context.Seller.Any(obj => obj.Id == seller.Id);
+
+            if (!sellerExists)
+            {
+                throw new NotFoundException("The seller was not found in our database");
+            }
+
+            try
+            {
+                _context.Update(seller);
+                _context.SaveChanges();
+            } catch(DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+
         }
     }
 }
