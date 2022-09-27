0be5fcc2f7793f03011c115ac0103c53c149da26
Michel Versiani <michel.pereira@audaztec.com.br>
2022-09-27 10:29:23 -0300

    New - Adding the grouped search feature

--- SalesWebMVC/Services/SalesRecordService.cs
+++ SalesWebMVC/Services/SalesRecordService.cs
@@ -37,5 +37,27 @@ namespace SalesWebMVC.Services
                 .OrderByDescending(x => x.Date)
                 .ToListAsync();
         }
+
+        public async Task< List< IGrouping<Department, SalesRecord> > > FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj; // OBJETO DE CONSULTA NO BANCO , TIPO UM QUERY BUILDER DO LARAVEL.
+
+            if (minDate.HasValue)
+            {
+                result = result.Where(query => query.Date >= minDate.Value);
+            }
+
+            if (maxDate.HasValue)
+            {
+                result = result.Where(query => query.Date <= maxDate.Value);
+            }
+
+            return await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .GroupBy(x => x.Seller.Department)
+                .ToListAsync();
+        }
     }
 }
